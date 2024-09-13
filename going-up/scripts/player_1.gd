d0a99c488ae62a076c89cf95c3b57eabc600b3fb
Matias <garciamatias.90@gmail.com>
2024-09-13 16:44:37 -0300

    Double Jump Mecanic

--- going-up/scripts/player_1.gd
+++ going-up/scripts/player_1.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+var ACTUAL_JUMPS = 0
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 enum State {Idle, Run, Jump, Stand}
@@ -17,16 +17,19 @@ func _physics_process(delta: float) -> void:
 	_running(delta)
 	_animations(delta)
 	
-	move_and_slide()
-
+	move_and_slide() ## sinceramente no se que hace eso, si alguno sabe me avisa
+	
+## Funcion para la gravedad
 func _gravedad(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+## Funcion idle
 func _idle(delta: float) -> void:
 	if is_on_floor():
 		current_state = State.Idle
-
+		
+## Funcion para moverse
 func _running(delta:float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -40,18 +43,18 @@ func _running(delta:float) -> void:
 		else:
 			current_state = State.Jump
 
-
-
-
-
-
-
+## Funcion para saltos
 func _jumping(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		current_state = State.Jump
+		ACTUAL_JUMPS = 1
+	elif Input.is_action_just_pressed("ui_up") and ACTUAL_JUMPS == 1:
+		velocity.y = JUMP_VELOCITY
+		ACTUAL_JUMPS += 1 
+		
 
-
+## Funcion para animacions
 func _animations(delta: float) -> void:
 	if current_state == State.Run:
 		animated_sprite_2d.play("run")
