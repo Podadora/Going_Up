9fd8c3ce373f995d828308baee4d7e5b986dc725
Matias <garciamatias.90@gmail.com>
2024-09-16 00:47:36 -0300

    Sprint
    quedo bastante feo, pero hay que pulirlo, parece que se teletransporta

--- going-up/scripts/player_1.gd
+++ going-up/scripts/player_1.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var ACTUAL_JUMPS = 0
-const SPEED = 300.0
+const sprint_speed = normal_speed * 12
+const normal_speed = 400.0
 const JUMP_VELOCITY = -400.0
-enum State {Idle, Run, Jump, Stand}
+enum State {Idle, Run, Jump, Sprint}
 var current_state
 
 func _ready() -> void:
@@ -16,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	_jumping(delta)
 	_running(delta)
 	_animations(delta)
-	
+	_sprint(delta)
 	move_and_slide() ## sinceramente no se que hace eso, si alguno sabe me avisa
 	
 ## Funcion para la gravedad
@@ -26,22 +27,33 @@ func _gravedad(delta: float) -> void:
 
 ## Funcion idle
 func _idle(delta: float) -> void:
-	if is_on_floor():
+	if velocity.x == 0 and is_on_floor():
 		current_state = State.Idle
 		
 ## Funcion para moverse
 func _running(delta:float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * normal_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, normal_speed)
 	if direction != 0 :
 		animated_sprite_2d.flip_h = false if direction > 0 else true
 		if is_on_floor():
 			current_state = State.Run
 		else:
 			current_state = State.Jump
+			
+## Funcion para sprintear
+func _sprint(delta:float) -> void:
+
+	if Input.is_action_just_pressed("sprint"):
+		current_state = State.Sprint
+		if animated_sprite_2d.flip_h :
+			velocity.x = -sprint_speed
+		else :
+			velocity.x = sprint_speed
+
 
 ## Funcion para saltos
 func _jumping(delta: float) -> void:
@@ -60,8 +72,8 @@ func _animations(delta: float) -> void:
 		animated_sprite_2d.play("run")
 	elif current_state == State.Jump:
 		animated_sprite_2d.play("jump")
-	elif current_state == State.Stand:
-		animated_sprite_2d.play("stand")
+	elif current_state == State.Sprint:
+		animated_sprite_2d.play("sprint")
 	elif current_state == State.Idle:
 		animated_sprite_2d.play("idle")
 
